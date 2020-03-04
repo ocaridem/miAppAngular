cf9182722a043edccf413006f2a1aef05e415839
java508 <java508@SINENSIA.EDU>
2020-03-04 18:19:44 +0100

    cambios 20200304 A

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,10 +22,11 @@ import { TrimestrePipe } from './pipes/trimestre.pipe';
 import { List1Component } from './components/list1/list1.component';
 import { List2Component } from './components/list2/list2.component';
 import { VendingMachineComponent } from './components/vending-machine/vending-machine.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent, CalculatorComponent, RelojComponent, BoxComponent, ConversorGradosComponent, ConversorDivisasComponent, PipeShowcaseComponent, CharCounterPipe, ReverserPipe, TrimestrePipe, List1Component, List2Component, VendingMachineComponent], //aqui van los "Component"
-  imports:      [BrowserModule,FormsModule], //FormsModule es para hacer 2-way databinding
+  imports:      [BrowserModule,FormsModule, BrowserAnimationsModule], //FormsModule es para hacer 2-way databinding
   providers:    [ConversorTemperaturaService], //aqui van los "Injectable"
   bootstrap:    [AppComponent]
 })
